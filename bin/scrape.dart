4389970b3e52e55373da3febad412e1db0b9fe14
Scott Hyndman <scott@madewithfelt.com>
2021-02-25 02:02:12 -0500

    Move the anchor link to an independent element

--- bin/scrape.dart
+++ bin/scrape.dart
@@ -299,7 +299,7 @@ String nodeToHtml(DocNode node, {required int level}) {
       final idAttr = headingLevel < 5 ? ' id="$id"' : '';
       sb.writeln('''
         <h$headingLevel$idAttr>
-          <a href="#$id">${node.label}</a>
+          ${node.label}<a class="anchor-link" href="#$id"></a>
         </h$headingLevel>
       ''');
     } else if (headingLevel <= 5) {
@@ -382,21 +382,11 @@ String createPageHtml({required String title, required String content}) {
           color: #0a0a0a;
         }
 
-        h1 > a:hover,
-        h2 > a:hover,
-        h3 > a:hover,
-        h4 > a:hover,
-        h5 > a:hover {
-          color: #0a0a0a;
-        }
-
-        h1 > a:hover::after,
-        h2 > a:hover::after,
-        h3 > a:hover::after,
-        h4 > a:hover::after,
-        h5 > a:hover::after {
-          display: inline-block;
-          content: '';
+        h1:hover > .anchor-link,
+        h2:hover > .anchor-link,
+        h3:hover > .anchor-link,
+        h4:hover > .anchor-link,
+        h5:hover > .anchor-link {
           background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' version='1.1' width='16' height='16' aria-hidden='true'%3E%3Cpath fill-rule='evenodd' d='M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z'%3E%3C/path%3E%3C/svg%3E");
           width: 16px;
           height: 16px;
